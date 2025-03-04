e1e47b58b0bbae4b5cec4a0f7ff9111e6065cab6
alodev <brayan.quialo@gmail.com>
2025-03-03 20:47:32 -0600

    base project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Monitor from './modules/monitor/components/Monitor'
+
+
+function App() {
+
+  return (
+    <>
+      <Monitor />
+    </>
+  )
+}
+
+export default App
---
+++ src/modules/monitor/components/Monitor.tsx
@@ -0,0 +1,53 @@
+import './Monitor.css';
+
+const trafficLights = [
+    { className: "traffic-light-north", idPrefix: "north" },
+    { className: "traffic-light-south", idPrefix: "south" },
+    { className: "traffic-light-east", idPrefix: "east" },
+    { className: "traffic-light-west", idPrefix: "west" }
+];
+
+const crosswalks = [
+    { className: "crosswalk-top" },
+    { className: "crosswalk-bottom" },
+    { className: "crosswalk-left" },
+    { className: "crosswalk-right" }
+];
+
+function Monitor() {
+    return (
+        <div className="intersection">
+            <div className="road road-horizontal"></div>
+            <div className="road road-vertical"></div>
+
+            <div className="lane-divider-horizontal-left"></div>
+            <div className="lane-divider-horizontal-right"></div>
+            <div className="lane-divider-vertical-top"></div>
+            <div className="lane-divider-vertical-bottom"></div>
+
+            {crosswalks.map((walk, index) => (
+                <div key={index} className={walk.className}>
+                    {[...Array(6)].map((_, i) => (
+                        <div key={i} className="crosswalk-stripe"></div>
+                    ))}
+                </div>
+            ))}
+
+            {trafficLights.map(({ className, idPrefix }) => (
+                <div key={idPrefix} className={`traffic-light ${className}`}>
+                    {['red', 'yellow', 'green'].map(color => (
+                        <div key={color} className="light" id={`${idPrefix}-${color}`}></div>
+                    ))}
+                </div>
+            ))}
+            <div className="car">
+                <div className="window-front"></div>
+                <div className="window-back"></div>
+                <div className="headlight headlight-left"></div>
+                <div className="headlight headlight-right"></div>
+            </div>
+        </div>
+    );
+}
+
+export default Monitor;
